04b64956c9a91a43b2067849b7340cc62de66ba9
Perry D <pd@kidpub.com>
2020-11-11 15:03:11 -0500

    plugging mongo into node

---
+++ 10NovMongoAndNode/mongoDemo/routes/mongoRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../mongoCx');
+
+router.route('/')
+.post(async (req, res) => {
+//    let theName = req.body.name;
+    let mongo = await db.getDB('cs412');
+//    let results = await mongo.collection('names').insert({name: theName});
+    let results = await mongo.collection('names').insert(req.body);
+
+    res.send(`Inserted ${req.body.name}`);
+})
+
+    .get(async (req, res) => {
+        let mongo = await db.getDB('cs412');
+        let results = await mongo.collection('names').find({name: 'Bob'}).toArray();
+        if (!results.length)  {
+            res.send('Not found');
+        }
+        else {
+            res.json(results);
+        }
+    })
+    router.get('/:name', async (req, res) => {
+        let mongo = await db.getDB('cs412');
+        let results = await mongo.collection('names').find({name: req.params.name}).toArray();
+        if (!results.length)  {
+            res.send('Not found');
+        }
+        else {
+            res.json(results);
+        }
+    })
+
+module.exports = router;
