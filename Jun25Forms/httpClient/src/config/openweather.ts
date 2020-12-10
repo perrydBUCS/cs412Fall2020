bb4bb8b4f8b6a095c408aa19ab71d2ce92da5695
Perry D <pd@kidpub.com>
2020-12-10 15:20:38 -0500

    forms four ways

---
+++ Jun25Forms/httpClient/src/config/openweather.ts
@@ -0,0 +1,4 @@
+export let config = {
+  apiKey: '6d13b989d2175d527ad89638956aec97',
+  baseURL: 'http://api.openweathermap.org/data/2.5/weather?q='
+}
---
+++ Jun25Forms/httpClient/src/config/yy.ts
@@ -0,0 +1,16 @@
+class A {
+  a: string;
+  b: {
+    c: number;
+    d: number;
+  }
+};
+let xx : A = {
+  a: 'hey',
+  b: {
+    c: 5,
+    d: 6
+  }
+}
+
+console.log(`${xx.b.c}`)
